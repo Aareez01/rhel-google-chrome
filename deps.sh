4f46015cb2a24f3602952872ebd3203344fbe10c
Aareez01 <124786944+Aareez01@users.noreply.github.com>
2024-04-27 04:59:34 +0500

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -2,15 +2,14 @@ dnf update
 dnf install wget
 dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 dnf install http://repo.okay.com.mx/centos/8/x86_64/release/okay-release-1-5.el8.noarch.rpm --nogpgcheck
+dnf install https://pkgs.sysadmins.ws/el8/base/x86_64/raven-release-1.0.1-1.el8.noarch.rpm
 wget https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/c/chromium-common-123.0.6312.122-1.el8.x86_64.rpm
 dnf install chromium-common-123.0.6312.122-1.el8.x86_64.rpm
 wget https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/c/chromium-123.0.6312.122-1.el8.x86_64.rpm
 wget http://mirror.centos.org/centos/8-stream/AppStream/x86_64/os/Packages/qt5-qtbase-5.15.3-7.el8.x86_64.rpm
-dnf install qt5-qtbase-5.15.3-7.el8.x86_64.rpm
 dnf upgrade --nogpgcheck
 dnf install qt5-qtbase --nogpgcheck
 wget https://pkgs.sysadmins.ws/el8/extras/x86_64/pipewire-libs-1.0.3-1.el8.x86_64.rpm
-dnf install https://pkgs.sysadmins.ws/el8/base/x86_64/raven-release-1.0.1-1.el8.noarch.rpm
 dnf install http://mirror.centos.org/centos/8-stream/BaseOS/x86_64/os/Packages/libtdb-1.4.9-0.el8.x86_64.rpm
 dnf install http://mirror.centos.org/centos/8-stream/AppStream/x86_64/os/Packages/sound-theme-freedesktop-0.8-9.el8.noarch.rpm
 dnf install http://mirror.centos.org/centos/8-stream/AppStream/x86_64/os/Packages/libcanberra-0.30-18.el8.x86_64.rpm --nogpgcheck
